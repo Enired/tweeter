e6d292823eee338c350cf7893c1ce26f60e32d10
Derine Banis <d.banis.13@gmail.com>
2022-06-21 17:25:36 -0700

    Implement character counter logic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,21 +2,33 @@ $(()=>{
   //Code goes here
   const $userInput = $('#tweet-text');
   let composerCharCounter = $('.counter');
-  let charCount = composerCharCounter.val();
-  $userInput.on('input', (event) =>{
-    const inputType = event.originalEvent.inputType
-    if(inputType === 'deleteContentBackward'){
-      if(charCount < 140){
-        charCount++;
-        composerCharCounter.val(charCount)
-      }
-    }
-    else{
-      charCount--;
-      composerCharCounter.val(charCount)
+  $userInput.on('input', () =>{
+    // const inputType = event.originalEvent.inputType
+    // if(inputType === 'deleteContentBackward'){
+    //   if(charCount < 140){
+    //     charCount++;
+    //     composerCharCounter.val(charCount)
+    //   }
+    // }
+    // else{
+    //   charCount--;
+    //   composerCharCounter.val(charCount)
+    // }
+
+    const charMax = 140;
+    let charRemaining = charMax - $userInput.val().length
+    composerCharCounter.val(charRemaining)
+    
+    if(charRemaining < 0){
+      composerCharCounter.addClass('counter-over-limit')
     }
 
-    console.log(inputType)
+    if(charRemaining >=0){
+      composerCharCounter.removeClass('counter-over-limit')
+    }
+    
+
+    console.log(charRemaining)
     })
   
 })
